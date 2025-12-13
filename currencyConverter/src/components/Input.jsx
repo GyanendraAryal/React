f12c6939504fdad940199525e755788f0c3bde8b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2025-12-13 22:17:31 +0545

    Second commit

---
+++ currencyConverter/src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption = [],
+    selectCurrency=""
+}){
+    return (
+        <div>InputBox</div>
+    )
+}
+
+export default InputBox;
