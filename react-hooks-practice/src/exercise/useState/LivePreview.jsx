3d5c419f05f2dd67306ede5caa54fa16905aadd4
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-04 22:28:34 +0545

    added string-counter to the livepreview

--- react-hooks-practice/src/exercise/useState/LivePreview.jsx
+++ react-hooks-practice/src/exercise/useState/LivePreview.jsx
@@ -1,19 +1,39 @@
 import { useState } from "react";
 
-export default function LivePreview(){
+export default function LivePreview() {
     const [text, setText] = useState("");
 
-    return(
-        <div className="w-1/4 h-20 mt-5 ml-25 bg-amber-200 rounded-lg flex flex-col items-center justify-between" >
-            <input
-            className="w-full h-15 bg-white rounded-lg flex flex-col items-center justify-between border border-slate-500"            value={text}
-            placeholder="Type something..."
-            onChange={(e)=>setText(e.target.value )}
-            type="text" />
+    const charCount = text.length;
+    const wordCount = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
+    const isEmpty = text.trim() === "";
+    const tooLong = charCount > 50;
 
-            <p className="mt-4 mr-20 text-xl text-blue-600">
-                Preview: {text}
-            </p>
+
+    return (
+        <div className="w-1/2 h-100 bg-blue-400 flex flex-wrap flex-col items-center">
+            <div className="w-1/2 h-50 mt-5 bg-amber-200 rounded-lg flex flex-col flex-wrap items-center justify-center gap-10" >
+                <input
+                    className="w-full h-15 bg-white rounded-lg flex flex-col items-center justify-between border border-slate-500" value={text}
+                    placeholder="Type something..."
+                    onChange={(e) => setText(e.target.value)}
+                    type="text" />
+
+                <p className="mr-20 flex flex-wrap text-xl text-blue-600">
+                    Preview: {text}
+                </p>
+            </div>
+            <div className="mt-4 space-y-2 text-lg">
+                <p>Total characters:{charCount}</p>
+                <p>Word count:{wordCount}</p>
+                <p>Is empty?:{isEmpty ? "Yes" : "No"}</p>
+
+                {tooLong && (
+                    <p className="text-red-600 font-semibold">
+                        Warning: More than 50 characters!
+                    </p>
+                )}
+            </div>
         </div>
+
     )
 }
