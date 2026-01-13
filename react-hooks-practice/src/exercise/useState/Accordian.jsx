555adbf3248fbb98f78b08a772d156bbc0d7d214
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-13 15:43:00 +0545

    multiple questions on accordian

--- react-hooks-practice/src/exercise/useState/Accordian.jsx
+++ react-hooks-practice/src/exercise/useState/Accordian.jsx
@@ -13,6 +13,26 @@ export const Accordian = () => {
                 {active ? <div className='h-15 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
             </div>
             }
+
+            {active ? <div className='h-30 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-30 flex items-center'>What is your name?</p>
+                {active ? <div className='h-30 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div> : 
+            <div className='h-15 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-15 flex items-center'>What is your name?</p>
+                {active ? <div className='h-15 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div>
+            }
+            
+            {active ? <div className='h-30 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-30 flex items-center'>What is your name?</p>
+                {active ? <div className='h-30 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div> : 
+            <div className='h-15 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-15 flex items-center'>What is your name?</p>
+                {active ? <div className='h-15 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div>
+            }
         </div>
     )
 }
