3cc6d292809f09b423fc3dd6e49c4f82f735ee50
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-13 15:38:34 +0545

    created accordian

--- react-hooks-practice/src/App.jsx
+++ react-hooks-practice/src/App.jsx
@@ -1,11 +1,12 @@
 import ToDoList from "./exercise/useState/ToDoList"
 import ToggleText from "./exercise/useState/ToggleText";
 import LivePreview from "./exercise/useState/LivePreview";
+import { Accordian } from "./exercise/useState/Accordian";
 function App() {
 
   return (
     <>
-      <LivePreview />
+      <Accordian />
     </>
   )
 }
---
+++ react-hooks-practice/src/exercise/useState/Accordian.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+export const Accordian = () => {
+    const [active, setActive] = useState(false);
+    return (
+        <div className='h-60 w-full bg-slate-600 flex flex-col items-center justify-center'>
+            <div className='text-2xl text-white'>Accordian</div>
+            {active ? <div className='h-30 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-30 flex items-center'>What is your name?</p>
+                {active ? <div className='h-30 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div> : 
+            <div className='h-15 w-4/5 bg-amber-600 flex flex-col  items-center'>
+                <p onClick={()=>setActive(!active)} className='text-white h-15 flex items-center'>What is your name?</p>
+                {active ? <div className='h-15 w-full bg-amber-200 flex justify-center items-center '>My Name is Accordian!!</div> : ""}
+            </div>
+            }
+        </div>
+    )
+}
