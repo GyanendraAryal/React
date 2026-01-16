28609eea946e640ef22d96b1f3bdaf1a8ca3616c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-16 22:02:59 +0545

    added Card from DevUi

--- TailwindAndProps/src/App.jsx
+++ TailwindAndProps/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import Props from './components/Props'
+import Card from './components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let myDetail = {
+    Roll:1,
+    name:"normie",
+    age:100
+  }
   return (
     <>
-     <Props/>
+      <Props />
+      <div className='w-full h-full flex flex gap-4 items-center justify-center'>
+        <Card creator="Ich heise gyanendra" value={myDetail} field="learning" />
+        <Card />
+      </div>
     </>
   )
 }
