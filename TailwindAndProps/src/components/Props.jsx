2dbd494d27caf7f740e67a05fc562c37bb619fb0
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-16 21:07:40 +0545

    added gitignore and created TailwindAndProps

---
+++ TailwindAndProps/src/components/Props.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const Props = () => {
+    return (
+        <>
+            <div className='h-16 max-w-[500px] mx-[30%] bg-slate-500 text-2xl text-white text-center'>Ich lerne Tailwind Und Props</div>
+        </>
+    )
+}
+
+export default Props
