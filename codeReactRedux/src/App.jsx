ffc2e9ed9e7a91ee5c10ea35de359d8de2ac7968
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-25 16:13:17 +0100

    used tailwindcss

--- codeReactRedux/src/App.jsx
+++ codeReactRedux/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    App
+    <h1 className='text-2xl text-green-500 h-10 w-max-3/2 bg-blue-300 ' >Hello Learning ReactRedux from CODE HELP</h1>
     </>
   )
 }
