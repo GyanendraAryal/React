a971250e17fa8c48c526624cbe74daf6cb8be769
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-28 12:23:55 +0100

    add NavLink navbar component

---
+++ w3schoolReactRouter/src/components/NavLInk.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export const NavLInk = () => {
+  return (
+    <>
+    <div className='h-10 w-2/4  bg-slate-500 text-2xl text-white'>
+        Ich komme aus NabLink!!
+        Ich komme aus NabLink1!!
+        Ich komme aus NabLink2!!
+    </div>
+    </>
+  )
+}
