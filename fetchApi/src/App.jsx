9a01e5153650ee14bafda5c7a3141ee17978849c
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-01-28 13:21:22 +0100

    updated app

--- fetchApi/src/App.jsx
+++ fetchApi/src/App.jsx
@@ -3,12 +3,17 @@ import { useState,useEffect } from 'react'
 function App() {
 
   const getUsersData = async ()=>{
-   const url = 'https://dummyjson.com/users';
+   try {
+    const url = 'https://jsonplaceholder.typicode.com/users';
    let response = await fetch(url);
-   response = await response.json;
+   response = await response.json();
    console.log(response);
+   setUsersData(response)
+   } catch (error) {
+    console.error(error)
+   }
   }
-  const [count, setCount] = useState(0)
+  const [usersData, setUsersData] = useState([])
   useEffect(()=>{
     getUsersData();
   },[])
@@ -17,6 +22,9 @@ function App() {
     <>
       <div>
         <h1>Fetch data from API</h1>
+        {usersData.map((user) => (
+        <h2 key={user.id}>{user.name}</h2>
+      ))}
       </div>
     </>
   )
