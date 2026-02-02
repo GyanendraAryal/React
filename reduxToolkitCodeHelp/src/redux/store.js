ad01031f8ae31f7c0643f946b5e8487b30b1c12b
Gyanendra Aryal <gyanendraaryal4@gmail.com>
2026-02-02 21:06:54 +0545

    configure redux store

---
+++ reduxToolkitCodeHelp/src/redux/store.js
@@ -0,0 +1,13 @@
+import {configureStore} from '@reduxjs/toolkit'
+
+//Step1: Created Store
+//Step2: Wrap the App in the main.js with the store as a prop. 
+//Step3: Created Slice
+//Step4: Created Store
+
+
+
+
+export const store = configureStore({
+    reducer:{}
+})
